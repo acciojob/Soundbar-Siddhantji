cc76c9ea811b72eb7bebca24777eba38039d6eed
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-05-03 19:08:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function playSound(soundName) {
+  var audio = new Audio('sounds/' + soundName + '.mp3');
+  audio.play();
+}
+
+// Function to stop all sounds
+function stopAllSounds() {
+  var sounds = document.querySelectorAll('audio');
+  sounds.forEach(sound => {
+    sound.pause();
+    sound.currentTime = 0;
+  });
+}
