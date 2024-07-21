cfc751dbac80dd2c2b7c48c7beee55ab584db1d3
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-07-22 03:29:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-//your JS code here. If required.
-const buttons = document.getElementById('buttons');
-const sounds = {};
-
-buttons.addEventListener('click', function(e) {
-  const sound = e.target.dataset.sound;
-  if (sound) {
-    if (sounds[sound]) {
-      sounds[sound].play();
-    } else {
-      const audio = new Audio(`sounds/${sound}.mp3`);
-      sounds[sound] = audio;
-      audio.play();
-    }
-  } else if (e.target.classList.contains('stop')) {
-    for (const sound in sounds) {
-      sounds[sound].pause();
-      sounds[sound].currentTime = 0;
-    }
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
   }
-});
+
+function stop() {
+  var audio = document.getElementById("audio");
+  audio.pause();
+  audio.currentTime = 0;
+}
